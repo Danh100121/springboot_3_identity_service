78605379981e626e7e9078d331ddb1f6c8d11726
PHAM TIEN DANH(Pham Tien Danh - Steve) <danh100121@lgcns.com>
2025-02-12 09:12:01 +0700

    refactor code

--- src/main/java/com/danhpahm/identity_service/mapper/PermissionMapper.java
+++ src/main/java/com/danhpahm/identity_service/mapper/PermissionMapper.java
@@ -1,14 +1,9 @@
 package com.danhpahm.identity_service.mapper;
 
 import com.danhpahm.identity_service.dto.request.PermissionRequest;
-import com.danhpahm.identity_service.dto.request.UserCreationRequest;
-import com.danhpahm.identity_service.dto.request.UserUpdateRequest;
 import com.danhpahm.identity_service.dto.response.PermissionResponse;
-import com.danhpahm.identity_service.dto.response.UserResponse;
 import com.danhpahm.identity_service.entity.Permission;
-import com.danhpahm.identity_service.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface PermissionMapper {
